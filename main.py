704ef5a05c477ccf29843a278e1c35196fbd3737
JuanCarlosAlmarioBabilonia <160683492+JuanCarlosAlmarioBabilonia@users.noreply.github.com>
2024-03-08 08:45:59 -0500

    8/3/2024 progreso jardineria

--- main.py
+++ main.py
@@ -5,4 +5,21 @@ import modules.getEmpleados as empleado
 import modules.getPedido as pedido
 import modules.getPago as pago
 import modules.getProducto as producto
-print (tabulate(pago.getAllFormasPago()))
+def menu():
+    print("""
+MENU PRINCIPAL
+1. Clientes
+2. Oficina
+3. Empleados
+4. Pedidos
+""")
+    op=int(input("Seleccione una de las opciones: "))
+    if(op==1):
+        clientes.menu()
+    elif(op==2):
+        oficina.menu()
+    elif(op==3):
+        empleado.menu()
+    elif(op==4):
+        pedido.menu()
+menu()
--- modules/getClientes.py
+++ modules/getClientes.py
@@ -1,25 +1,37 @@
-import storage.cliente as cli 
+import storage.cliente as cli
+from tabulate import tabulate
 def getAllClientesName():
     clienteName = list()
     for val in cli.cliente:
         codigoName = dict({
-        "codigo_cliente": val.get("codigo_cliente"),
-        "nombre_cliente": val.get("nombre_cliente")
+        "Codigo": val.get("codigo_cliente"),
+        "Nombre del Cliente": val.get("nombre_cliente")
         })
         clienteName.append(codigoName)
     return clienteName
 def getOneClientCodigo(codigo):
     for val in cli.cliente:
         if(val.get("codigo_cliente") == codigo):
-            return{
-        "codigo_cliente": val.get("codigo_cliente"),
-        "nombre_cliente": val.get("cliente_cliente")   
-            } 
+            return[{
+        "Codigo del Cliente": val.get("codigo_cliente"),
+        "Nombre del Cliente": val.get("nombre_cliente")   
+            }]
 def getAllClientCreditoCiudad(limiteCredit, ciudad):
     clienteCredic = list()
     for val in cli.cliente:
         if(val.get("limite_credito") >= limiteCredit and val.get("ciudad") == ciudad):
-            clienteCredic.append(val)
+            clienteCredic.append({
+                "Codigo":val.get("codigo_cliente"),
+                "Nombre del Ciente":val.get("nombre_cliente"),
+                "Director":f"{val.get('nombre_contacto')} {val.get('apellido_contacto')}",
+                "Telefono":val.get("telefono"),
+                "Fax":val.get("fax"),
+                "Direcciones":f"{val.get('ciudad')} {val.get('region')} {val.get('pais')} {val.get('codigo_postal')}",
+                "Origen":val.get("codigo_cliente"),
+                "Codigo del Asesor":val.get("codigo_empleado_rep_ventas"),
+                "Credito":val.get("limite_credito")
+            })
+
     return clienteCredic
 def getAllClientPaisRegionCiudad(pais, region = None, ciudad = None):
     clientZone = list()
@@ -86,3 +98,20 @@ def getAllNombreClienteEspañol():
                 }
             )
     return clienteEspañol
+def menu():
+    print("""
+REPORTES DE LOS CLIENTES
+1. Obtener todos los clientes (codigo y nombre)
+2. Obtener un cliente por el codigo
+3. Obtener toda la informacion de un cliente segun su limite de credito y ciudad a la que pertenece (ejem: 3000.0 - San Francisco)
+""")
+    op=int(input("Seleccione una de las opciones: "))
+    if(op==1):
+        print(tabulate(getAllClientesName(),headers="keys",tablefmt="grid"))
+    elif(op==2):
+        codigoCliente=int(input("Ingrese el codigo del cliente: "))
+        print(tabulate(getOneClientCodigo(codigoCliente),headers="keys",tablefmt="grid"))
+    elif(op==3):
+        limite=float(input("Ingrese el limite de credito de los clientes que desea visualizar: "))
+        ciudad=input("Ingrese el nombre de la ciudad de la que desea filtrar los clientes: ")
+        print(tabulate(getAllClientCreditoCiudad(limite,ciudad),headers="keys",tablefmt="grid"))
